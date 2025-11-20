5edd3c7fecd3c65abf321b3266475211e67bd6b9
sierrapablo <proyectos@sierrapablo.dev>
2025-11-20 22:13:26 +0100

    🔨 fix bugs : fix cors deps and deprecated import rules

--- src/controllers/versionController.js
+++ src/controllers/versionController.js
@@ -1,6 +1,10 @@
-import pkg from "../../package.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
 
 const getVersion = (_req, res) => {
+  const packagePath = path.resolve(process.cwd(), "package.json");
+  const pkg = JSON.parse(fs.readFileSync(packagePath, "utf-8"));
+
   res.status(200).json({
     name: pkg.name,
     version: pkg.version,
