091f283bd6a454069b602b5e68ed2c91a6e439ef
sierrapablo <proyectos@sierrapablo.dev>
2025-11-20 20:49:39 +0100

    🔨 fix bugs : Fix CORS deps and ESModules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import cors from "cors";
 import express from "express";
-import testRoute from "./routes/testRoute";
+import testRoute from "./routes/testRoute.js";
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
 app.use("/", testRoute);
 
 app.listen(port, () => {
